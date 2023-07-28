23ed5b50bf611d11315d76b9ecebb91eb693a101
Luu Vu <vu7178@gmail.com>
2023-07-29 01:29:52 +0700

    style CartPage

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -9,25 +9,25 @@ const Cart = () => {
       <h2>Shopping Cart</h2>
       {cart.cartItems.length === 0 ? (
         <div className="cart-empty">
-          <p>Your cart is empty</p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-arrow-left"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fillRule="evenodd"
-              d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
-            />
-          </svg>
-          <div className="cart-empty__link">
-            <Link to="/">
+          <span>Your cart is empty</span>
+          <Link to="/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-arrow-left"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+              />
+            </svg>
+            <div className="cart-empty__link">
               <span>Tiếp tục mua sắm nào</span>
-            </Link>
-          </div>
+            </div>
+          </Link>
         </div>
       ) : (
         <div className="cart-table">
@@ -53,7 +53,7 @@ const Cart = () => {
                 </div>
                 <div className="cart-item__quantity">
                   <button>-</button>
-                  <input type="text" value={item.cartQuantity} />
+                  <div className="cart-item__count">{item.cartQuantity}</div>
                   <button>+</button>
                 </div>
                 <div className="cart-item__total">
